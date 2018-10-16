a15eed4b7290c03bf2ebd47870937bb59116e16f
callmedadaxin <554091944@qq.com>
2018-10-16 14:40:48 +0800

    add mobile styles

--- client/components/Sidebar/index.js
+++ client/components/Sidebar/index.js
@@ -1,21 +1,35 @@
 import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import avatar from '../../../avatar.jpg'
 import './index.scss'
 
 export default class Sidebar extends PureComponent {
-  static propTypes = {
-
+  state = {
+    show: false
   }
-
+  toggleMenu = () => {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+  hideMenu = () => {
+    this.setState({
+      show: false
+    })
+  }
   render() {
+    const { show } = this.state
     return (
-      <aside className="left-menu">
+      <aside className={`left-menu ${show ? 'left-menu-show' : ''}`}>
+        <button className="left-menu__button" onClick={this.toggleMenu}>
+          <span className="left-menu__button-bar"></span>
+          <span className="left-menu__button-bar"></span>
+          <span className="left-menu__button-bar"></span>
+        </button>
         <div className="left-menu__logo">
-          <img className="left-menu__logo-content" src={avatar} alt=""/>
+          <img className="left-menu__logo-content" src={avatar}/>
         </div>
-        <ul className="left-menu__list">
+        <ul className="left-menu__list" onClick={this.hideMenu}>
           <li className="left-menu__list-item"><Link to="/">Posts</Link></li>
           <li className="left-menu__list-item"><Link to="/archives">Archives</Link></li>
           <li className="left-menu__list-item"><Link to="/tags">Tags</Link></li>
--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ import logger from 'koa-logger'
 import router from './route'
 import favicon from 'koa-favicon'
 import path from 'path'
-// const cors = require('koa2-cors')
+const cors = require('koa2-cors')
 
 const app = new Koa()
 
-// app.use(cors())
+app.use(cors())
 
 app.use(bodyParser())
 app.use(logger())
