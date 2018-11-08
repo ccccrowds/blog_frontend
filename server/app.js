aabea58443d30d632542e2573ae0d2e6a561a728
callmedadaxin <554091944@qq.com>
2018-11-08 16:18:23 +0800

    fix koa

--- server/app.js
+++ server/app.js
@@ -4,15 +4,33 @@ import logger from 'koa-logger'
 import router from './route'
 import favicon from 'koa-favicon'
 import path from 'path'
-const cors = require('koa2-cors')
+import views from 'koa-views'
+import serve from 'koa-static'
+// const cors = require('koa2-cors')
+import ssrMiddleWare from './middleware/server-render'
+
+const resolve = p => path.resolve(__dirname, p)
+const staticRoot = resolve('../dist')
 
 const app = new Koa()
 
-app.use(cors())
+// app.use(cors())
+app.use(views(staticRoot, { map: { html: 'ejs' } }))
+
+app.use(ssrMiddleWare)
+
+app.use(favicon(path.join(__dirname, '../favicon.ico')))
+
+app.use(serve(staticRoot, {
+  // one month cache for prod
+  index: 'index.js',
+  maxage: 2592000000,
+  gzip: true,
+}))
+
 
 app.use(bodyParser())
 app.use(logger())
-app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
 app.use(router.routes())
   .use(router.allowedMethods())
--- server/index.js
+++ server/index.js
@@ -1,24 +1,9 @@
-import views from 'koa-views'
-import serve from 'koa-static'
 import path from 'path'
 import Loadable from 'react-loadable'
 
 import config from './config'
 import app from './app'
-import ssrMiddleWare from './middleware/server-render'
 
-const resolve = p => path.resolve(__dirname, p)
-const staticRoot = resolve('../dist')
-
-app.use(views(staticRoot, { map: { html: 'ejs' } }))
-
-app.use(serve(staticRoot, {
-  index: 'index.js',
-  maxage: 60800,
-  gzip: true
-}))
-
-app.use(ssrMiddleWare)
 
 const { port } = config
 
